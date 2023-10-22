f15b45780bd92de60a1aa923d8898459fc20cea0
Tarsis <br.tmvdl@gmail.com>
2023-10-22 12:25:16 -0300

    Updating API files

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,19 @@
 const { Database } = require('@brtmvdl/database')
 const { Client, Events, GatewayIntentBits } = require('discord.js')
-
-const { texts } = require('./texts.json')
+const pack = require('./package.json')
 
 const { TOKEN } = require('./config.js')
 const { MessageLog } = require('./models')
 
 const db = new Database(process.env.DATA_PATH)
+
+const { Broker } = require('./libs/broker/index.js')
+
+const broker = new Broker()
+
+broker.message('Olá, gente!', (m) => m.reply(`Olá, meu nome é ${pack.nickname}`))
+
+broker.message('/clock', (m) => m.reply(`Agora é ${new Date()}.`))
 
 const client = new Client({
   intents: [
@@ -16,26 +23,11 @@ const client = new Client({
   ]
 })
 
-client.once(Events.ClientReady, client => console.log(`Client ready: ${client.user.tag}`))
-
-client.on(Events.Raw, (ev) => console.log('Raw', ev))
-
 client.on(Events.MessageCreate, (message) => {
   const message_log = new MessageLog(message)
   db.in('messages').new().writeMany(message_log.toJSON())
 
-  const text = texts.find((text) => {
-    return text.when.every((w) => {
-      switch (true) {
-        case w['message.0.content'] === message.content: return true
-      }
-
-      return false
-    })
-  })
-
-  if (text) message.reply(text.then)
-  else console.log({ message })
+  broker.run(message)
 })
 
 client.login(TOKEN)
