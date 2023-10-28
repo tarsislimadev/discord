0f3757eae33800784409f462ae8f61710c89cc8a
Tarsis <br.tmvdl@gmail.com>
2023-10-28 12:16:54 -0300

    Updating API files

--- src/api/libs/broker/index.js
+++ src/api/libs/broker/index.js
@@ -3,12 +3,30 @@ class Broker {
   messages = []
 
   message(when, then) {
-    this.messages.push({ when, then })
+    this.messages.push({ when, then, type: 'message' })
+    return this
+  }
+
+  regexp(when, then) {
+    this.messages.push({ when, then, type: 'regexp' })
+    return this
+  }
+
+  command(when, then) {
+    this.messages.push({ when, then, type: 'command' })
     return this
   }
 
   run(message) {
-    const current = this.messages.find((m) => m.when == message.content)
+    const current = this.messages.find((m) => {
+      switch (m.type) {
+        case 'message': return m.when === message.content
+        case 'regexp': return (new RegExp(m.when)).test(message.content)
+        case 'command': return message.content.substring(0, ('/' + m.when).length) == '/' + m.when
+      }
+
+      return false
+    })
 
     if (!(current === undefined)) current.then(message)
 
