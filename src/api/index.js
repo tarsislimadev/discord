e8854a4b3ee5c453f0be154ef0e4e9920b7fe523
Tarsis <br.tmvdl@gmail.com>
2024-03-14 16:08:37 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -4,37 +4,19 @@ const { Client, Events } = require('discord.js')
 const messages = require('./messages.js')
 const { TOKEN } = require('./config.js')
 const { MessageLog } = require('./models')
+
 const db = new Database(process.env.DATA_PATH)
-const { Audier } = require('./libs/audier')
-const audier = new Audier(process.env.DATA_PATH)
-const audioPlayer = new AudioPlayer({})
-
 const client = new Client({ intents: require('./intents.js') })
-const { createAudioResource, joinVoiceChannel, AudioPlayer } = require('@discordjs/voice')
 
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const now = Date.now()
-
-  console.log({ now })
-
-  audier.mp3(`Datetime: ${now}`, now).then((filename) => {
-    const connection = joinVoiceChannel({
-      channelId: interaction.channel.id,
-      guildId: interaction.channel.guild.id,
-      adapterCreator: interaction.channel.guild.voiceAdapterCreator,
-    })
-
-    connection.subscribe(audioPlayer)
-    audioPlayer.play(createAudioResource(fs.createReadStream(filename)))
-  }).catch((err) => console.error(err))
-})
+// client.on(Events.InteractionCreate, (interaction) => {
+//   if (!interaction.isChatInputCommand()) return;
+//   const now = Date.now()
+//   console.log({ now })
+// })
 
 client.on(Events.MessageCreate, (message) => {
   const message_log = new MessageLog(message)
   db.in('messages').new().writeMany(message_log.toJSON())
-
   messages.run(message)
 })
 
